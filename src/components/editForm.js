7ff4a188c30c4d7abfdcd78e529d27cbb9d858a7
Ann Hillis <andriellerhillis@gmail.com>
2019-06-11 08:45:08 -0500

    Incorporated the form-group componenet into the form component

--- src/components/editForm.js
+++ src/components/editForm.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
+import FormGroup from './form-group';
 
 class EditForm extends Component {
     render(){
         return (
             <form className="col-12 col-md-6" id="source-form">
-                <div className="form-group">
-                    <label for="title" className="col-form-label">Title</label>
-                    <input type="text" className="form-control form-control-sm" name="title"></input>
-                </div>
-                <div className="form-group">
-                    <label for="num-authors">Authors:</label>
-                    <select className="form-control">
-                        <option>One</option>
-                        <option>Two</option>
-                        <option>Three or more</option>
-                    </select>
-                </div>
+                <FormGroup name="title" />
+                <hr></hr>
                 <div className="author-group">
+                    <div className="form-group">
+                        <label for="num-authors">Authors:</label>
+                        <select className="form-control">
+                            <option>One</option>
+                            <option>Two</option>
+                            <option>Three or more</option>
+                        </select>
+                    </div>
                     <div className="form-group">
                         <label for="author" className="col-form-label">Author</label>
                         <input type="text" className="form-control form-control-sm" name="author"></input>
                     </div>
-                    <div className="form-group">
-                        <label for="publisher" className="col-form-label">Publisher</label>
-                        <input type="text" className="form-control form-control-sm" name="publisher"></input>
-                    </div>
                 </div>
-                <div className="form-group">
-                    <label for="author" className="col-form-label">Year</label>
-                    <input type="text" className="form-control form-control-sm" name="year"></input>
-                </div>
+                <FormGroup name="publisher" />
+                <FormGroup name="year" />
+                
             </form>
         )
     }
