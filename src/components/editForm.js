6b4beae8dd31546c6d6ce35570ab0dba5fc853d1
Ann Hillis <andriellerhillis@gmail.com>
2019-06-13 13:03:11 -0500

    Added the onClick function\

--- src/components/editForm.js
+++ src/components/editForm.js
@@ -3,7 +3,10 @@ import FormGroup from './form-group';
 import Authors from './authors';
 
 class EditForm extends Component {
-    
+    onClick(e){
+        e.preventDefault();
+        console.log(e.target);
+    }
     render(){
         return (
             <form className="col-12 col-md-6" id="source-form">
@@ -14,6 +17,7 @@ class EditForm extends Component {
                 <FormGroup name="year" />     
                 <input type="submit" 
                         className="btn btn-primary" 
+                        onClick={this.onClick.bind(this)}
                         value="Submit"/>
             </form>
         )
