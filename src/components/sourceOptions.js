4615583df450c718d250a87602ac9f8bbe8dfabc
Ann Hillis <andriellerhillis@gmail.com>
2019-06-13 10:15:45 -0500

    Refactored the sourceOptions component to handle an array of authors

--- src/components/sourceOptions.js
+++ src/components/sourceOptions.js
@@ -1,11 +1,13 @@
 import  React, { Component } from 'react';
 
 class sourceOption extends Component {
-    render() {
-        const {id, title, author} = this.props.source;
+    render() {        
+        const {id, authors, title} = this.props.source;
+        const {firstName, lastName} = authors[0];
+
         return (
             <option value={id}>
-                {title} by {author.firstName} {author.lastName}
+                {title} by {firstName} {lastName} {authors.length === 1 ? ' ' : 'et al'}
             </option>
         );
     }
