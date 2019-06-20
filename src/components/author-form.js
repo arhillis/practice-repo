bd2bcf792f24935c87e89993a3b93c9a24337173
Ann Hillis <andriellerhillis@gmail.com>
2019-06-20 18:17:55 -0500

    Putting the props in the constuctor

--- src/components/author-form.js
+++ src/components/author-form.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import uuid from 'uuid';
 
 class AuthorForm extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
             id: uuid.v4(),
             firstName: '',
