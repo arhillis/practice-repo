1a30ba9bc46d0d0b37ef8362815d02460bb4e937
Ann Hillis <andriellerhillis@gmail.com>
2019-06-20 08:29:29 -0500

    Changed the ID and formatted the JSX a little better

--- src/components/year-select.js
+++ src/components/year-select.js
@@ -9,8 +9,12 @@ class YearSelect extends Component{
         }
 
         return (
-            <select className="form-control" id="exampleFormControlSelect1">  
-                {years.map(year => (<option key={year}>{year}</option>))}
+            <select 
+                className="form-control" 
+                id="yearSelect">  
+                    {years.map(year => 
+                        (<option key={year}>{year}</option>)
+                    )}
             </select>
         );
 
