92cf845c56b51dc046a09b47320623aecf44abba
Ann Hillis <andriellerhillis@gmail.com>
2019-06-20 14:12:56 -0500

    Added the case for when a source has three or more authors

--- src/components/source.js
+++ src/components/source.js
@@ -12,12 +12,20 @@ class Source extends React.Component{
                     {lastName}, {firstName}. <cite>{title}</cite>. {publisher}, {year}.
                 </li>
             )
-        }else{
+        }else if(authors.length === 2){
             const [author1, author2] = authors;
             
             return (
                 <li>
                     {author1.lastName}, {author1.firstName} and {author2.firstName} {author2.lastName}. <cite>{title}</cite>. {publisher}, {year}.
+                </li>
+            )
+        }else{
+            const {firstName, lastName} = authors[0];
+
+            return (
+                <li>
+                    {lastName}, {firstName} et al. <cite>{title}</cite>. {publisher}, {year}.
                 </li>
             )
         }
