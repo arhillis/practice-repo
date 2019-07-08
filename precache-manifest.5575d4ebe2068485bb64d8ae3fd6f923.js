9f7201ecc5dc3423c9aad285b4e3156d6b174a9c
Ann Hillis <andriellerhillis@gmail.com>
2019-07-08 10:05:10 -0500

    Updates

---
+++ precache-manifest.5575d4ebe2068485bb64d8ae3fd6f923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d309c4f8308e82380c3d3e88cb35f83",
+    "url": "/works-cited-maker/index.html"
+  },
+  {
+    "revision": "6b41be18867e8424e870",
+    "url": "/works-cited-maker/static/css/main.cfb70dac.chunk.css"
+  },
+  {
+    "revision": "50db3f79255210a5e6bb",
+    "url": "/works-cited-maker/static/js/2.e43bdeaa.chunk.js"
+  },
+  {
+    "revision": "6b41be18867e8424e870",
+    "url": "/works-cited-maker/static/js/main.c686b6b6.chunk.js"
+  },
+  {
+    "revision": "00e62996721df97bd5a4",
+    "url": "/works-cited-maker/static/js/runtime~main.cf511972.js"
+  }
+]);
